f3aff42343c60949144cbe3bc90c70ca26a94bf3
Parth Patel <parth_patel@comcast.com>
2020-09-18 19:45:13 -0400

    add form to enter course

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CoursesPage() {
+    const [course, setCourse] = useState({
+      title: "",
+    });
+
+    function handleChange({ target }) {
+        setCourse({ ...course, [target.name]: target.value });
+    }
+
+    function submitHandler(event) {
+        event.preventDefault();
+        console.log("course", course);
+        alert("This is called!!!");
+    }
+
     return(
+        <form onSubmit={submitHandler}>
         <p>Courses</p>
+        <h3>Add course</h3>
+        <input name="title" type="text" onChange={handleChange} value={course.title} />
+
+        <input type="submit" value="Save" />
+        </form>
     );
 }
 
