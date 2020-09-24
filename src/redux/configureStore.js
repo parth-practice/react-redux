c90bf0a02f00a164b2d64ae556123d47a60c70ea
Parth Patel <parth_patel@comcast.com>
2020-09-24 13:28:36 -0400

    add boiler plate for redux

---
+++ src/redux/configureStore.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+
+import { rootReducer } from './reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+
+export default function configureStore(initialState) {
+    const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //add support for redux dev tool
+
+    return createStore(rootReducer, initialState, composeEnhancer(applyMiddleware(reduxImmutableStateInvariant())));
+}
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import combineReducer from 'redux';
+
+import courses from './courseReducer';
+
+const rootReducer = combineReducer ({
+    courses
+});
+
+export default rootReducer;
